4ce9a4d046044f4143d01605e2f4d17829f3f363
raparisonbryan <raparisonbryan@yahoo.fr>
2025-02-10 11:10:45 +0100

    init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import {ReactNode} from "react";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Classements",
+  description: "Générateur de classements",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body
+        className={`${inter.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
